4ae691ddc47f2698b1f9b6a40a8a5325dfa407a5
zhaotong <zhaotong1@lenovo.com>
2017-08-30 18:52:41 +0800

    greenDao的基本使用

--- app/src/main/java/com/lenovo/service/basicpubliclibrary/StorageActivity.java
+++ app/src/main/java/com/lenovo/service/basicpubliclibrary/StorageActivity.java
@@ -6,11 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.lenovo.service.basicpubliclibrary.greendao.GreenDaoDemoActivity;
 import com.lenovo.service.basicpubliclibrary.ormlite.OrmLiteActivity;
 
-public class StorageActivity extends AppCompatActivity implements View.OnClickListener{
+public class StorageActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView ormlite;
+    private TextView greenDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +20,26 @@ public class StorageActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_storage);
 
         ormlite = (TextView) findViewById(R.id.ormlite);
+        greenDao = (TextView) findViewById(R.id.greenDao);
 
         setOnClickListener();
     }
 
-    private void setOnClickListener(){
+    private void setOnClickListener() {
         ormlite.setOnClickListener(this);
+        greenDao.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.ormlite:
                 Intent intent = new Intent(this, OrmLiteActivity.class);
                 startActivity(intent);
                 break;
-            default:
+            case R.id.greenDao:
+                Intent green = new Intent(this, GreenDaoDemoActivity.class);
+                startActivity(green);
                 break;
         }
     }
