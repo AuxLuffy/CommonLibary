46eea29b5c68c7a1f4db9f6f8bc0d31e5efc6579
wyj <zhee_long@163.com>
2017-08-31 14:18:38 +0800

    Multitype—Android复杂的列表视图新写法

---
+++ app/src/main/java/com/lenovo/service/basicpubliclibrary/multitype/bilibili/PostsAdapter.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2016 drakeet. https://github.com/drakeet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lenovo.service.basicpubliclibrary.multitype.bilibili;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lenovo.service.basicpubliclibrary.R;
+
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * @author drakeet
+ */
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
+
+    private List<Post> posts = Collections.emptyList();
+
+
+    public void setPosts(@NonNull List<Post> posts) {
+        this.posts = posts;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+            .inflate(R.layout.item_horizontal_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.cover.setImageResource(post.coverResId);
+        holder.title.setText(post.title);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @NonNull
+        ImageView cover;
+        @NonNull
+        TextView title;
+
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            cover = (ImageView) itemView.findViewById(R.id.cover);
+            title = (TextView) itemView.findViewById(R.id.title);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), String.valueOf(getAdapterPosition()),
+                        Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/lenovo/service/basicpubliclibrary/multitype/multi_select/SquareViewBinder.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016 drakeet. https://github.com/drakeet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lenovo.service.basicpubliclibrary.multitype.multi_select;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lenovo.service.basicpubliclibrary.R;
+
+import java.util.Set;
+
+import me.drakeet.multitype.ItemViewBinder;
+
+import static java.lang.String.valueOf;
+
+/**
+ * @author drakeet
+ */
+public class SquareViewBinder extends ItemViewBinder<Square, SquareViewBinder.ViewHolder> {
+
+    private final Set<Integer> selectedSet;
+
+
+    public SquareViewBinder(Set<Integer> selectedSet) { this.selectedSet = selectedSet; }
+
+
+    @NonNull
+    @Override
+    protected ViewHolder onCreateViewHolder(
+            @NonNull LayoutInflater inflater, @NonNull ViewGroup parent) {
+        View root = inflater.inflate(R.layout.item_square, parent, false);
+        return new ViewHolder(root);
+    }
+
+
+    @Override
+    protected void onBindViewHolder(@NonNull ViewHolder holder, @NonNull Square square) {
+        holder.square = square;
+        holder.squareView.setText(valueOf(square.number));
+        holder.squareView.setSelected(square.isSelected);
+    }
+
+
+    public Set<Integer> getSelectedSet() {
+        return selectedSet;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView squareView;
+        private Square square;
+
+
+        ViewHolder(final View itemView) {
+            super(itemView);
+            squareView = (TextView) itemView.findViewById(R.id.square);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemView.setSelected(square.isSelected = !square.isSelected);
+                    if (square.isSelected) {
+                        selectedSet.add(square.number);
+                    } else {
+                        selectedSet.remove(square.number);
+                    }
+                }
+            });
+        }
+    }
+}
