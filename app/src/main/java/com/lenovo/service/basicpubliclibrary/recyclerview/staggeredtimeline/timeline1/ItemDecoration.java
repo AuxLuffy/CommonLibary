697856b1f0752ba119ac2c5b9a0844e3d6fed9fe
袁东华 <1348474384@qq.com>
2017-09-17 18:35:25 +0800

    添加注释

--- app/src/main/java/com/lenovo/service/basicpubliclibrary/recyclerview/staggeredtimeline/timeline1/ItemDecoration.java
+++ app/src/main/java/com/lenovo/service/basicpubliclibrary/recyclerview/staggeredtimeline/timeline1/ItemDecoration.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.lenovo.service.basicpubliclibrary.R;
+import com.lenovo.service.basicpubliclibrary.utils.LogUtil;
 
 
 /**
@@ -19,21 +20,21 @@ import com.lenovo.service.basicpubliclibrary.R;
 
 public class ItemDecoration extends RecyclerView.ItemDecoration {
     private Context mContext;
+    //间距
     private int distance;
     //时光轴节点图片
-    private Drawable drawable;
+    private Drawable nodeDrawable;
     //时光轴的纵线
     private  Drawable verticalLine;
     //时光轴的横线
     private Drawable horizontalLine;
+    //itemView距离屏幕的左右间距
+    private int distanceBetweenScreenAndItem;
 
 
-    public ItemDecoration(Context context, int distance) {
+    public ItemDecoration(Context context) {
         mContext = context;
-        this.distance = distance;
-        verticalLine = ContextCompat.getDrawable(mContext, R.drawable.gray_line);
-        drawable = ContextCompat.getDrawable(mContext, R.drawable.time);
-        horizontalLine = ContextCompat.getDrawable(mContext, R.drawable.horizontal_line);
+
     }
 
     @Override
@@ -46,41 +47,60 @@ public class ItemDecoration extends RecyclerView.ItemDecoration {
         } else if (parent.getChildAdapterPosition(view) == 1) {
             outRect.top = 4 * distance;
         }
-
+        //itemView的索引是偶数的时候重置outRect.left ,也就是设置itemView的左间距
         if (parent.getChildAdapterPosition(view) % 2 == 0) {
-            outRect.left = 20;
+            outRect.left = distanceBetweenScreenAndItem;
         } else {
-            outRect.right = 20;
+            //itemView的索引是奇数的时候重置outRect.right ,也就是设置itemView的右间距
+            outRect.right = distanceBetweenScreenAndItem;
         }
     }
 
     @Override
     public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
-        drawHorizontal(c, parent);
-        drawVertical(c, parent);
+        drawVerticalLine(c, parent);
+        drawHorizontalLine(c, parent);
     }
 
-    public void drawHorizontal(Canvas c, RecyclerView parent) {
+    /**
+     * 绘制纵线
+     * @param c
+     * @param parent
+     */
+    public void drawVerticalLine(Canvas c, RecyclerView parent) {
+        //获取RecyclerView的top
         final int top = parent.getPaddingTop();
+        //获取RecyclerView的bottom
         final int bottom = parent.getHeight() - parent.getPaddingBottom();
+        //获取RecyclerView的宽度
         final int parentWidth = parent.getMeasuredWidth();
 
         verticalLine.setBounds(parentWidth / 2 - 1, top, parentWidth / 2 + 1,bottom);
         verticalLine.draw(c);
     }
 
-    public void drawVertical(Canvas c, RecyclerView parent) {
+    /**
+     * 绘制横线
+     * @param c
+     * @param parent
+     */
+    public void drawHorizontalLine(Canvas c, RecyclerView parent) {
+        //获取RecyclerView的宽度
         final int parentWidth = parent.getMeasuredWidth();
-
+        //获取当前屏幕itemView的个数
         final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {//-1最后一个不画
+        for (int i = 0; i < childCount; i++) {
+            //-1最后一个不画
             final View child = parent.getChildAt(i);
-
-            final int top = child.getTop() + (child.getBottom() - child.getTop()) / 2 - drawable.getIntrinsicHeight() / 2;
-            final int bottom = top + drawable.getIntrinsicHeight();
-
+            //时间节点的top
+            final int top = child.getTop() + (child.getBottom() - child.getTop()) / 2 - nodeDrawable.getIntrinsicHeight() / 2;
+            //时间节点的bottom
+            final int bottom = top + nodeDrawable.getIntrinsicHeight();
+            //横线的left
             int horizontalLineLeft = child.getRight();
-            int horizontalLineRight = parentWidth / 2;
+            //横线的right
+            int horizontalLineRight = parentWidth / 2-nodeDrawable.getIntrinsicWidth()/2;
+            //横线的top
             int horizontalLineTop = child.getTop() + (child.getBottom() - child.getTop()) / 2;
 
             if (child.getLeft()<parentWidth/2) {
@@ -91,11 +111,51 @@ public class ItemDecoration extends RecyclerView.ItemDecoration {
             horizontalLine.setBounds(horizontalLineLeft, horizontalLineTop, horizontalLineRight, horizontalLineTop + 2);
             horizontalLine.draw(c);
 
-            int drawableLeft = parentWidth / 2 - drawable.getIntrinsicWidth() / 2;
-            int drawableRight = parentWidth / 2 + drawable.getIntrinsicWidth() / 2;
+            int drawableLeft = parentWidth / 2 - nodeDrawable.getIntrinsicWidth() / 2;
+            int drawableRight = parentWidth / 2 + nodeDrawable.getIntrinsicWidth() / 2;
 
-            drawable.setBounds(drawableLeft, top, drawableRight, bottom);
-            drawable.draw(c);
+            nodeDrawable.setBounds(drawableLeft, top, drawableRight, bottom);
+            nodeDrawable.draw(c);
         }
+    }
+
+    public int getDistanceBetweenScreenAndItem() {
+        return distanceBetweenScreenAndItem;
+    }
+
+    public void setDistanceBetweenScreenAndItem(int distanceBetweenScreenAndItem) {
+        this.distanceBetweenScreenAndItem = distanceBetweenScreenAndItem;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public Drawable getNodeDrawable() {
+        return nodeDrawable;
+    }
+
+    public void setNodeDrawable(Drawable nodeDrawable) {
+        this.nodeDrawable = nodeDrawable;
+    }
+
+    public Drawable getVerticalLine() {
+        return verticalLine;
+    }
+
+    public void setVerticalLine(Drawable verticalLine) {
+        this.verticalLine = verticalLine;
+    }
+
+    public Drawable getHorizontalLine() {
+        return horizontalLine;
+    }
+
+    public void setHorizontalLine(Drawable horizontalLine) {
+        this.horizontalLine = horizontalLine;
     }
 }
--- app/src/main/java/com/lenovo/service/basicpubliclibrary/recyclerview/staggeredtimeline/timeline1/TimeLine1Activity.java
+++ app/src/main/java/com/lenovo/service/basicpubliclibrary/recyclerview/staggeredtimeline/timeline1/TimeLine1Activity.java
@@ -1,12 +1,16 @@
 package com.lenovo.service.basicpubliclibrary.recyclerview.staggeredtimeline.timeline1;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 
 
 import com.lenovo.service.basicpubliclibrary.R;
+import com.lenovo.service.basicpubliclibrary.shadowmasking.utils.ScreenUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,21 +21,22 @@ import java.util.List;
  * @time:2017/9/17 下午5:46
  */
 public class TimeLine1Activity extends AppCompatActivity {
-    RecyclerView mRecyclerView;
-    int[] resId = {
+    private RecyclerView mRecyclerView;
+    private int[] resId = {
             R.drawable.image_1,
-//            R.drawable.image_2,
+            R.drawable.image_2,
             R.drawable.image_3,
             R.drawable.image_4,
             R.drawable.image_5,
-            R.drawable.image_6,
             R.drawable.image_7,
+            R.drawable.image_6,
             R.drawable.image_8
     };
 
     List<Item> mList = new ArrayList<>();
     TimeLineAdapter mAdapter;
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,10 +44,18 @@ public class TimeLine1Activity extends AppCompatActivity {
         setContentView(R.layout.activity_staggered_timeline1);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        //创建瀑布流Layout
         mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
-        mRecyclerView.addItemDecoration(new ItemDecoration(this, 100));
+        //创建ItemDecoration
+        ItemDecoration itemDecoration = new ItemDecoration(this);
+        itemDecoration.setDistanceBetweenScreenAndItem(ScreenUtils.dp2px(this,10));
+        itemDecoration.setDistance(ScreenUtils.dp2px(this,30));
+        itemDecoration.setNodeDrawable(getDrawable(R.drawable.time));
+        itemDecoration.setHorizontalLine(ContextCompat.getDrawable(this, R.drawable.horizontal_line));
+        itemDecoration.setVerticalLine(ContextCompat.getDrawable(this, R.drawable.gray_line));
+        mRecyclerView.addItemDecoration(itemDecoration);
 
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < resId.length; i++) {
             Item item = new Item();
             item.setResId(resId[i]);
             item.setText("我从未见过如此厚颜无耻之人");
