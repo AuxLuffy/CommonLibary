318926cd44fd3e2a857d5dd6d9cfb1f0de4d9670
jiaoyuming <ymjiaoa@isoftstone>
2017-09-28 17:09:49 +0800

    集成阿里热修复

--- app/src/main/java/com/lenovo/service/basicpubliclibrary/App.java
+++ app/src/main/java/com/lenovo/service/basicpubliclibrary/App.java
@@ -2,7 +2,9 @@ package com.lenovo.service.basicpubliclibrary;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.multidex.MultiDex;
+import android.widget.Toast;
 
 import com.baidu.crabsdk.CrabSDK;
 import com.blankj.ALog;
@@ -15,6 +17,9 @@ import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
+import com.taobao.sophix.PatchStatus;
+import com.taobao.sophix.SophixManager;
+import com.taobao.sophix.listener.PatchLoadStatusListener;
 import com.tencent.smtt.sdk.QbSdk;
 
 import org.litepal.LitePal;
@@ -105,6 +110,8 @@ public class App extends SugarApp {
             refWatcher=  LeakCanary.install(this);
         }
         QbSdk.initX5Environment(this,null);
+        // queryAndLoadNewPatch不可放在attachBaseContext 中，否则无网络权限，建议放在后面任意时刻，如onCreate中
+        SophixManager.getInstance().queryAndLoadNewPatch();
     }
 
     public static Context getContext() {
@@ -121,6 +128,32 @@ public class App extends SugarApp {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
+        // initialize最好放在attachBaseContext最前面，初始化直接在Application类里面，切勿封装到其他类
+        try {
+            SophixManager.getInstance().setContext(this)
+                    .setAppVersion((getPackageManager().getPackageInfo(getPackageName(),0)).versionName)
+                    .setAesKey(null)
+                    .setEnableDebug(true)
+                    .setPatchLoadStatusStub(new PatchLoadStatusListener() {
+                        @Override
+                        public void onLoad(final int mode, final int code, final String info, final int handlePatchVersion) {
+                            // 补丁加载回调通知
+                            if (code == PatchStatus.CODE_LOAD_SUCCESS) {
+                                // 表明补丁加载成功
+                                Toast.makeText(App.this , "补丁加载成功成功", Toast.LENGTH_SHORT).show();
+                            } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
+                                Toast.makeText(App.this , "补丁下载成功请重启应用", Toast.LENGTH_SHORT).show();
+                                // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
+                                // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
+                            } else {
+                                // 其它错误信息, 查看PatchStatus类说明
+                                Toast.makeText(App.this , "热修复"+info, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }).initialize();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         MultiDex.install(this);
     }
 
