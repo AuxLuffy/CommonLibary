8e1d5133de0e36d5909bf1960eedfc75a3297935
xuxiaowei <13693540693@163.com>
2017-09-30 17:48:47 +0800

    监听手机摇一摇和振动DEMO

---
+++ app/src/main/java/com/lenovo/service/basicpubliclibrary/bezier/ShakeActivity.java
@@ -0,0 +1,158 @@
+package com.lenovo.service.basicpubliclibrary.bezier;
+
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lenovo.service.basicpubliclibrary.R;
+
+import java.lang.ref.WeakReference;
+
+public class ShakeActivity extends AppCompatActivity implements SensorEventListener{
+
+    private static final int START_SHAKE = 0x1;
+    private static final int AGAIN_SHAKE = 0x2;
+    private static final int END_SHAKE = 0x3;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometerSensor;
+    private Vibrator mVibrator;
+    private SoundPool mSoundPool;
+    private int mWeiChatAudio;
+
+    //记录摇动状态
+    private boolean isShake = false;
+    private MyHandler mHandler;
+
+
+    private static class MyHandler extends Handler {
+        private WeakReference<ShakeActivity> mReference;
+        private ShakeActivity mActivity;
+        private int count = 0;
+        private TextView tv_count;
+        public MyHandler(ShakeActivity activity, TextView tv_count) {
+            mReference = new WeakReference<ShakeActivity>(activity);
+            if (mReference != null) {
+                mActivity = mReference.get();
+            }
+            this.tv_count = tv_count;
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case START_SHAKE:
+                    //This method requires the caller to hold the permission VIBRATE.
+                    mActivity.mVibrator.vibrate(300);
+                    //发出提示音
+                    mActivity.mSoundPool.play(mActivity.mWeiChatAudio, 1, 1, 0, 0, 1);
+                    tv_count.setText(++count+"");
+                    break;
+                case AGAIN_SHAKE:
+                    mActivity.mVibrator.vibrate(300);
+                    break;
+                case END_SHAKE:
+                    //整体效果结束, 将震动设置为false
+                    mActivity.isShake = false;
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置只竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_shake);
+
+        TextView tv_countqq = (TextView) findViewById(R.id.tv_countqq);
+
+        mHandler = new MyHandler(this, tv_countqq);
+        //初始化SoundPool
+        mSoundPool = new SoundPool(1, AudioManager.STREAM_SYSTEM, 5);
+        mWeiChatAudio = mSoundPool.load(this, R.raw.weichat_audio, 1);
+        //获取Vibrator震动服务
+        mVibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //获取 SensorManager 负责管理传感器
+        mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
+        if (mSensorManager != null) {
+            //获取加速度传感器
+            mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if (mAccelerometerSensor != null) {
+                mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_UI);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        // 务必要在pause中注销 mSensorManager
+        // 否则会造成界面退出后摇一摇依旧生效的bug
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(this);
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int type = event.sensor.getType();
+
+        if (type == Sensor.TYPE_ACCELEROMETER) {
+            //获取三个方向值
+            float[] values = event.values;
+            float x = values[0];
+            float y = values[1];
+            float z = values[2];
+
+            if ((Math.abs(x) > 17 || Math.abs(y) > 17 || Math
+                    .abs(z) > 17) && !isShake) {
+                isShake = true;
+                // TODO: 2016/10/19 实现摇动逻辑, 摇动后进行震动
+                Thread thread = new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        try {
+                            //开始震动 发出提示音 展示动画效果
+                            mHandler.obtainMessage(START_SHAKE).sendToTarget();
+                            Thread.sleep(500);
+                            //再来一次震动提示
+                            mHandler.obtainMessage(AGAIN_SHAKE).sendToTarget();
+                            Thread.sleep(500);
+                            mHandler.obtainMessage(END_SHAKE).sendToTarget();
+
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                thread.start();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
